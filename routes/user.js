3bd371ea6a2817b567a9706f32054c424a997fd0
hhh <h18270711373@163.com>
2021-03-04 09:55:54 +0800

    修改登录接口

--- routes/user.js
+++ routes/user.js
@@ -62,63 +62,63 @@ router.post("/register", function (req, res, next) {
 
 // 登录
 router.post("/login", function (req, res, next) {
-  const { appid, secret, code } = req.body;
-  request(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`, function (error, response, body) {
-    console.error("error:", error); // Print the error if one occurred
-    console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    console.log("body:", body); // Print the HTML for the Google homepage.
+  const { account, password, appid, secret, code } = req.body;
+  // request(`https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`, function (error, response, body) {
+  //   console.error("error:", error); // Print the error if one occurred
+  //   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+  //   console.log("body:", body); // Print the HTML for the Google homepage.
+  // });
+  const secretOrPrivateKey = "myapp";
+  const token = jwt.sign({ account, password }, secretOrPrivateKey, {
+    expiresIn: 60 * 60,
   });
-  // const secretOrPrivateKey = "myapp";
-  // const token = jwt.sign({ account, password }, secretOrPrivateKey, {
-  //   expiresIn: 60 * 60,
-  // });
-  // queryUserList(next, function (userList) {
-  //   if (!userList.length) {
-  //     res.json({
-  //       code: 400,
-  //       status: false,
-  //       message: "该账号不存在！",
-  //     });
-  //     // next();
-  //     return;
-  //   }
+  queryUserList(next, function (userList) {
+    if (!userList.length) {
+      res.json({
+        code: 400,
+        status: false,
+        message: "该账号不存在！",
+      });
+      // next();
+      return;
+    }
 
-  //   let user = userList.find((item) => item.account === account);
-  //   if (!user) {
-  //     res.json({
-  //       code: 400,
-  //       status: false,
-  //       message: "该账号不存在！",
-  //     });
-  //   } else {
-  //     if (user.password === password) {
-  //       res.json({
-  //         code: 200,
-  //         data: {
-  //           token,
-  //         },
-  //         status: true,
-  //         message: "登录成功！",
-  //       });
-  //       db.query(
-  //         `UPDATE users SET token = '${token}' WHERE ACCOUNT = ${account} AND PASSWORD = ${password};`,
-  //         [],
-  //         function (results, fields) {
-  //           if (results.affectedRows) {
-  //             console.log("token存储成功！");
-  //           }
-  //         },
-  //         next
-  //       );
-  //     } else {
-  //       res.json({
-  //         code: 400,
-  //         status: false,
-  //         message: "密码错误",
-  //       });
-  //     }
-  //   }
-  // });
+    let user = userList.find((item) => item.account === account);
+    if (!user) {
+      res.json({
+        code: 400,
+        status: false,
+        message: "该账号不存在！",
+      });
+    } else {
+      if (user.password === password) {
+        res.json({
+          code: 200,
+          data: {
+            token,
+          },
+          status: true,
+          message: "登录成功！",
+        });
+        db.query(
+          `UPDATE users SET token = '${token}' WHERE ACCOUNT = ${account} AND PASSWORD = ${password};`,
+          [],
+          function (results, fields) {
+            if (results.affectedRows) {
+              console.log("token存储成功！");
+            }
+          },
+          next
+        );
+      } else {
+        res.json({
+          code: 400,
+          status: false,
+          message: "密码错误",
+        });
+      }
+    }
+  });
 });
 
 module.exports = router;
